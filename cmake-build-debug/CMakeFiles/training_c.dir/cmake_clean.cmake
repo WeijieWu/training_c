f1a62b956798f5468e789c83c9b3d4711b55d681
吴伟杰 <wu.weijie@qingniu.co>
2018-03-19 16:29:50 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/training_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/training_c.dir/main.cpp.o"
+  "training_c.pdb"
+  "training_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/training_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
